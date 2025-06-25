2067adf33ca1891790209f5a37ceb1646b5e86fc
Alejandro Álvarez Ayllón <alejandro.alvarez@sonarsource.com>
2025-06-25 14:51:15 +0200

    Fix

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
+#include <chrono>
 #include <iostream>
+#include <thread>
 
 int main() {
   std::cout << "OHNO" << std::endl;
-  return 1;
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+  return 0;
 }
